5caa5b65b7ef1d65548142855cbd4cd7dcc4f939
Sidd_0710 <siddheshwarchirkute2003@gmail.com>
2025-07-28 23:41:22 +0530

    Functions and Loops

---
+++ Day 1/var, let, const differences/Functions.js
@@ -0,0 +1,20 @@
+// what  why  how  
+// 
+// agr code likh diya to wo turant chal jayega
+
+// function add(a, b) {
+//     return a + b;
+// }
+// console.log(add(5, 10)); // Output: 15
+// this is called function declaration and statement
+
+
+// let fun = function(a, b) {
+//     return a + b;
+// }
+
+// console.log(fun(5, 10)); // Output: 15
+// these is called function expressions
+
+
+
---
+++ Day 1/var, let, const differences/Loops.js
@@ -0,0 +1,56 @@
+// //print the number from 1 to 10 using the loops
+
+
+// // for (let i= 1; i<=10; i++) {
+// //     console.log(i);
+// // }
+
+// //  print the number from 10 to 1  using the while loop
+
+// // for(let i=10; i>=1;i--) {
+// //     console.log(i);
+// // }
+
+
+// // let i = 10;
+// // while (i >= 1) {
+// //     console.log(i);
+// //     i--;
+// // }   real solution
+
+
+// // // print the  even number from 1 to 20 usiing  loop
+
+// // for (let i = 1;i <= 21; i++){
+
+// //     if (i % 2 === 0){
+// //         console.log(i);
+// //     }
+
+// // }
+
+// // print odd number from 1 to 15 using  a while loop
+
+
+// // let i = 1;
+// // while (i <= 15){
+// //     if (i %2 ===1){
+// //         console.log(i);
+// //     }
+// //     i ++;
+// // }
+
+// // print the table of 5 using a for loop
+
+// // for (let i = 1; i <= 10; i++) {
+// //     console.log(`5 x ${i} = ${5 * i}`);
+// // }
+
+
+// // print the sum of numbers from 1 to 100 using a loops
+
+// let sum = 0;
+// for (let i = 1;  i <= 100; i++) {
+//     sum += i;
+// }
+// console.log(`The sum of numbers from 1 to 100 is: ${sum}`); 
